121df6712295adf1d0cef22223ab5fa287677177
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-09-24 15:05:37 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,15 @@
 # 3. compile .java files into .class files with javac
 
 cd src/modelbuilderMk2
-/Applications/Processing.app/Contents/PlugIns/jdk-17.0.8+7/Contents/Home/bin/javac @./options $(find ./ -name "*.java")
+/Applications/Processing.app/Contents/PlugIns/jdk-17.0.8+7/Contents/Home/bin/javac -cp ":/Applications/Processing.app/Contents/Java/core.jar:../../libs/geomerative.jar:../../libs/commons-math3-3.6.1.jar:../../libs/commons-math3-3.6.1.jar:../../libs/commons-math3-3.6.1-tools.jar:../../libs/commons-math3-3.6.1-tests.jar:../../libs/commons-math3-3.6.1-test-sources.jar:../../libs/commons-math3-3.6.1-sources.jar:../../libs/commons-math3-3.6.1-javadoc.jar:../../libs/poly2tri.jar:../../libs/apacheCommonsMathRotations.jar:../../libs/modelbuilderMk2.jar" -d ../../lib $(find ./ -name "*.java")
 cd -
+cd lib
 
 # 4. compile .class files into a jar with jar cvf modelbuilderMk2.jar 
 
 jar cvf modelbuilderMk2.jar *
+cp modelbuilderMk2.jar ~/Documents/Processing/libraries/modelbuilderMk2/library/
+cd -
 
 # 5. move everything into the library structure
 
